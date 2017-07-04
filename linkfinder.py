096c0629fc13aa547ef050983399479f481d6eaa
Ibram Marzouk <0xibram@users.noreply.github.com>
2017-07-04 19:31:08 -0400

    check if file exists

--- linkfinder.py
+++ linkfinder.py
@@ -91,7 +91,8 @@ def parser_input(input):
         return (paths if len(paths) > 0 else parser_error('Input with wildcard does \
         not match any files.'))
     else:
-        return ["file:///%s" % os.path.abspath(input)]
+        path = "file:///%s" % os.path.abspath(input)
+        return [path if os.path.exists(path) else parser_error("file could not be found.")]
 
 # Send request using Requests
 
