e3ed83218b6c792519db46da79bab498a050e4cf
GerbenJavado <g.janssenvandoorn@gmail.com>
2017-07-18 15:39:00 +0200

    Added Burp save and cookies support

--- linkfinder.py
+++ linkfinder.py
@@ -8,17 +8,9 @@ import os
 os.environ["BROWSER"] = "open"
 
 # Import libraries
-import re
-import sys
-import glob
-import cgi
-import argparse
-import requests
-import urllib
+import re, sys, glob, cgi, argparse, requests, urllib, jsbeautifier, webbrowser, subprocess, base64, xml.etree.ElementTree
+
 from requests_file import FileAdapter
-import jsbeautifier
-import webbrowser
-import subprocess
 from string import Template
 from requests.packages.urllib3.exceptions import InsecureRequestWarning 
 
@@ -41,12 +33,12 @@ regex = re.compile(r"""
     |
 
     ([a-zA-Z0-9/]{1,}/              # Relative endpoint with /
-    [a-zA-Z0-9_\-/]{1,}\.[a-z]{1,4}    # Rest + extension
+    [a-zA-Z0-9_\-/]{1,}\.[a-z]{1,4} # Rest + extension
     (?:[\?|/][^"|']{0,}|))          # ? mark with parameters
 
     |
 
-    ([a-zA-Z0-9_\-]{1,}                # filename
+    ([a-zA-Z0-9_\-]{1,}             # filename
     \.(?:php|asp|aspx|jsp)          # . + extension
     (?:\?[^"|']{0,}|))              # ? mark with parameters
  
@@ -70,6 +62,12 @@ parser.add_argument("-r", "--regex",
                     help="RegEx for filtering purposes \
                     against found endpoint (e.g. ^/api/)",
                     action="store")
+parser.add_argument("-b", "--burp",
+                    help="",
+                    action="store_true")
+parser.add_argument("-c", "--cookies",
+                    help="Add cookies for authenticated JS files",
+                    action="store", default="")
 args = parser.parse_args()
 
 
@@ -86,13 +84,26 @@ def parser_input(input):
     '''
     Parse Input
     '''
+
+    # Method 1 - URL
     if input.startswith(('http://', 'https://',
                          'file://', 'ftp://', 'ftps://')):
         return [input]
 
+    # Method 2 - URL Inspector Firefox
     if input.startswith('view-source:'):
         return [input[12:]]
 
+    # Method 3 - Burp file
+    if args.burp:
+        jsfiles = []
+        items = xml.etree.ElementTree.fromstring(open(args.input, "r").read())
+        
+        for item in items:
+            jsfiles.append(base64.b64decode(item.find('response').text).decode('utf-8'))
+        return jsfiles
+
+    # Method 4 - Folder with a wildcard
     if "*" in input:
         paths = glob.glob(os.path.abspath(input))
         for index, path in enumerate(paths):
@@ -100,6 +111,7 @@ def parser_input(input):
         return (paths if len(paths) > 0 else parser_error('Input with wildcard does \
         not match any files.'))
 
+    # Method 5 - Local file
     path = "file://%s" % os.path.abspath(input)
     return [path if os.path.exists(input) else parser_error("file could not \
         be found.")]
@@ -116,6 +128,7 @@ def send_request(url):
         application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.8',
         'Accept-Encoding': 'gzip',
+        'Cookie': args.cookies
     }
 
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -125,22 +138,14 @@ def send_request(url):
     return content.text if hasattr(content, "text") else content.content
 
 
-def parser_file(url):
+def parser_file(content):
     '''
     Parse Input
     '''
-    try:
-        content = send_request(url)
-    except Exception as e:
-        parser_error("invalid input defined or SSL error: %s" % e)
-
+    
     # Beautify
-    if args.output != 'cli':
-        content = jsbeautifier.beautify(content)
-        items = re.findall(regex, content)
-    else:
-        items = re.findall(regex, content)
-        items = sorted(set(items))
+    content = jsbeautifier.beautify(content)
+    items = re.findall(regex, content)
         
     # Match Regex
     filtered_items = []
@@ -157,45 +162,18 @@ def parser_file(url):
 
     return filtered_items
 
-# Program
-files = parser_input(args.input)
-
-
-def cli_output():
+def cli_output(endpoints):
     '''
     Output to CLI
     '''
-    for file in files:
-        endpoints = parser_file(file)
-        for endpoint in endpoints:
-            print(urllib.unquote(cgi.escape(endpoint[1])).encode(
-                'ascii', 'ignore').decode('utf8'))
-
-# Output to HTML file
-for file in files:
-    endpoints = parser_file(file)
-    html = '''
-    <h1>File: <a href="%s" target="_blank" rel="nofollow noopener noreferrer">%s</a></h1>
-    ''' % (cgi.escape(file), cgi.escape(file))
-
     for endpoint in endpoints:
-        url = cgi.escape(endpoint[1])
-        string = "<div><a href='%s' class='text'>%s" % (
-            urllib.unquote(url).encode('ascii', 'ignore').decode('utf8'),
-            urllib.unquote(url).encode('ascii', 'ignore').decode('utf8')
-        )
-        string2 = "</a><div class='container'>%s</div></div>" % cgi.escape(
-            endpoint[0]
-        )
-        string2 = string2.replace(
-            cgi.escape(endpoint[1]),
-            "<span style='background-color:yellow'>%s</span>" %
-            cgi.escape(endpoint[1])
-        )
+        print(cgi.escape(endpoint[1]).encode(
+            'ascii', 'ignore').decode('utf8')) 
 
-        html += string + string2
-
-if args.output != 'cli':
+def html_save(html):
+    '''
+    Save as HTML file and open in the browser
+    '''
     hide = os.dup(1)
     os.close(1)
     os.open(os.devnull, os.O_RDWR)
@@ -203,7 +181,7 @@ if args.output != 'cli':
         s = Template(open('%s/template.html' % sys.path[0], 'r').read())
 
         text_file = open(args.output, "wb")
-        text_file.write(s.substitute(content=html).encode('utf-8'))
+        text_file.write(s.substitute(content=html).encode('utf8'))
         text_file.close()
 
         print("URL to access output: file://%s" % os.path.abspath(args.output))
@@ -217,5 +195,43 @@ if args.output != 'cli':
             due to exception: %s" % (args.output, e))
     finally:
         os.dup2(hide, 1)
-else:
-    cli_output()
+
+# Convert input to URLs or JS files
+urls = parser_input(args.input)  
+
+# Convert URLs to JS
+for url in urls:
+    if not args.burp:
+        try:
+            file = send_request(url)
+        except Exception as e:
+            parser_error("invalid input defined or SSL error: %s" % e)
+    else: 
+        file = url
+
+    endpoints = parser_file(file)
+    html = ''
+
+    if args.output == 'cli':
+        cli_output(endpoints)
+    else:
+
+        for endpoint in endpoints:
+            url = cgi.escape(endpoint[1])
+            string = "<div><a href='%s' class='text'>%s" % (
+                cgi.escape(url),
+                cgi.escape(url)
+            )
+            string2 = "</a><div class='container'>%s</div></div>" % cgi.escape(
+                endpoint[0]
+            )
+            string2 = string2.replace(
+                cgi.escape(endpoint[1]),
+                "<span style='background-color:yellow'>%s</span>" %
+                cgi.escape(endpoint[1])
+            )
+        
+            html += string + string2
+
+if args.output != 'cli':
+    html_save(html)
