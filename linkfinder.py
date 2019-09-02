67623f15db4c3351b9d9097031ddf1429753c857
alex1704 <kostenkoaleksander7@gmail.com>
2019-09-03 00:31:25 +0300

    Improve filename regex - recognize filenames with multiple dots in it.

--- linkfinder.py
+++ linkfinder.py
@@ -50,7 +50,7 @@ regex_str = r"""
 
     |
 
-    ([a-zA-Z0-9_\-]{1,}                 # filename
+    ([a-zA-Z0-9_\-.]{1,}                 # filename
     \.(?:php|asp|aspx|jsp|json|
          action|html|js|txt|xml)             # . + extension
     (?:\?[^"|']{0,}|))                  # ? mark with parameters
